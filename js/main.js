57783828284c3d712478e5a368ebd60bf8ca2479
sesmaluciana <sesmaluciana9@gmail.com>
2024-05-09 14:09:53 -0300

    desafio 3

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let lloviendo = true
+let nublado = true
+let pronostico = true
+
+if(!lloviendo || nublado ){
+    console.log("Lleva un paraguas");
+}else{
+    console.log("no lleves paraguas");
+}
